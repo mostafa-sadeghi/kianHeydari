31bb429669279e51d4bfeaeb5ff2f8f68bd12f8b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-12 14:59:16 +0330

    sadeghi

---
+++ s2_1.py
@@ -0,0 +1,97 @@
+# print("hello world")
+# print("hello", "welcome", "ok")
+# print("I want to print a double quote \"for some reason.\"")
+# print("The file is stored in C:\\new folder")
+
+# print("Hello.\nwelcome to python class.\nwelcome to pygame")
+
+# print("hi.\rhello")
+# print("hello\rhi")
+# print("Hello.\rwelcome to python class.\rwelcome to pygame")
+
+# x = 10
+# print(x)
+
+# print("x")
+# print("x=",x)
+
+# print(x)
+# x = x + 1
+# print(x)
+# x + 1
+# print(x)
+
+
+# x = 6
+# x = 5
+
+
+# print(x)
+
+# +     -     *      /       //       **       %
+
+# print(2+3)
+# print("2+3")
+# print(2+3*3)
+# print((2+3)*3)
+
+
+# average = (90 + 86 + 71 + 100 + 98) / 5
+# print(average)
+
+# name = input('enter you name: ')
+# family = input('enter your family: ')
+# print(name, family)
+
+# print(type(name))
+# print(type(family))
+# print(type(1))
+
+
+# number1 = int(input('enter first number: '))
+# number2 = int(input('enter second number: '))
+# print(number1 + number2)
+
+# print("ali"+"reza")
+
+#   +     -     *     /     //    %
+
+
+a = 14
+b = 5
+
+# if a > b:
+#     print("a is greater than b")
+#     print("blalallala")
+# print("outside")
+
+
+if a < b:
+    print("a is less than b")
+
+if a >= b:
+    print("a is greater than or equal to b")
+
+if a <= b:
+    print("a is lower than or equal to b")
+
+
+a = 3
+if a > 4:
+    print("a is greather than 4")
+elif a == 4:
+    print("a is equal to 4")
+else:
+    print("a is less than 4")
+
+
+# exercise 1:
+'''برنامه ای بنویسی که نام و سن کاربر را از ووردی دریافت نماید و در صورتی که
+سن او از هشت کمتر بود 
+ali is kid
+در صورتی که سن ا.و بین هشت و سیزده بود
+ali is junior
+در صورتی که سن او بین سیزده و هجده بود
+ali is teenager
+در صورتی که سن او از هجده بزرگتر بود
+ali is adult'''
